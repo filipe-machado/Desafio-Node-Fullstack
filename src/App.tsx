5da3036750585daf486ff99671b56dd3dcbf22b6
filipe-machado <ax-filipe@hotmail.com>
2024-06-12 22:32:55 -0400

    🎉 START
    Inicializando repositório com libs essenciais

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import "./App.css";
+import { Home } from "./app/Home";
+
+// https://www.youtube.com/watch?v=Lrj2RhSZ1L8
+function App() {
+  return (
+    <>
+      <Home />
+    </>
+  );
+}
+
+export default App;
